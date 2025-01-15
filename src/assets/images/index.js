291a7b2caa0de76fd5ffe03b255a93f28c4ee263
Oluwa-Laughter <ismakinde@gmail.com>
2025-01-15 11:33:18 +0100

    update tailwind config

---
+++ src/assets/images/index.js
@@ -0,0 +1,6 @@
+export { default as paper } from "./icon-paper.svg";
+export { default as rock } from "./icon-rock.svg";
+export { default as scissors } from "./icon-scissors.svg";
+export { default as bgTriangle } from "./bg-triangle.svg";
+export { default as rulesImg } from "./image-rules.svg";
+export { default as closeIcon } from "./icon-close.svg";
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  screens: {
+    sm: "28.75em",
+    md: "37.5em",
+    lg: "56.25em",
+  },
+
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3B4262",
+        secondary: "#DB2E4D",
+        grey: "#565468",
+        blue: "#2A45C2",
+        "border-color": "rgba(255, 255, 255, 0.29)",
+        "game-bg": "#0000001a",
+      },
+      backgroundImage: {
+        radiall:
+          "radial-gradient(134.34% 134.34% at 50% 0%, #1F3757 0%, #131537 100%);",
+        score: "linear-gradient(0deg, #F3F3F3 0%, #FFF 100%)",
+        triangle: 'url("assets/images/bg-triangle.svg")',
+        pentagon: 'url("assets/images/bg-pentagon.svg")',
+        "hover-bg": 'url("assets/images/bg-hover.svg")',
+      },
+      dropShadow: {
+        "text-sh": "0px 3px 3px rgba(0, 0, 0, 0.20)",
+      },
+      boxShadow: {
+        "box-sh": "0px 3px 3px rgba(0, 0, 0, 0.20)",
+      },
+      fontFamily: {
+        barlow: ["Barlow Semi Condensed", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
